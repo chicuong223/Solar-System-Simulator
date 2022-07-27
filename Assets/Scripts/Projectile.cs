d81a2b2d2c72511e83f6bb2cce30cfbab13af561
Chi Cuong Tang <chicuong.tang01@gmail.com>
2022-07-27 22:28:02 +0700

    Screenshots

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -7,7 +7,6 @@ using UnityEngine;
 /// </summary>
 public class Projectile : MonoBehaviour
 {
-    
     [Tooltip("The distance this projectile will move each second in meters.")]
     public float projectileSpeed = 3.0f;
 
@@ -17,9 +16,9 @@ public class Projectile : MonoBehaviour
     /// <summary>
     /// Description:
     /// Standard Unity function called once per frame
-    /// Inputs: 
+    /// Inputs:
     /// none
-    /// Returns: 
+    /// Returns:
     /// void (no return)
     /// </summary>
     private void Update()
@@ -30,21 +29,21 @@ public class Projectile : MonoBehaviour
     /// <summary>
     /// Description:
     /// Move the projectile in the direction it is heading
-    /// Inputs: 
+    /// Inputs:
     /// none
-    /// Returns: 
+    /// Returns:
     /// void (no return)
     /// </summary>
     private void MoveProjectile()
     {
         // move the transform
         transform.position = transform.position + transform.forward * projectileSpeed * Time.deltaTime;
-        
+
         // calculate the distance from the main camera
         float dist = Vector3.Distance(Camera.main.transform.position, transform.position);
 
         // if the distance is greater than the destroyDistance
-        if (dist>destroyDistance)
+        if (dist > destroyDistance)
         {
             Destroy(this.gameObject); // destroy the gameObject
         }
